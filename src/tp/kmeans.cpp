cca21114f76282678562e9ae3389e9ede244951e
mhourde <hourde.maxence@gmail.com>
2026-02-26 19:00:24 +0100

    tiens connard

--- src/tp/kmeans.cpp
+++ src/tp/kmeans.cpp
@@ -19,27 +19,152 @@ auto div(double a, double b){
     }
 }
 
+auto dist2_5 = [](const float* a, const float* b) -> float {
+    float s = 0.f;
+    for (int j = 0; j < 5; ++j) {
+        float d = a[j] - b[j];
+        s += d * d;
+    }
+    return s;
+};
+
+/**
+ * K-means "perso" (Lloyd) pour segmentation.
+ *
+ * - img : image d'entrée (CV_8UC1, CV_8UC3, ou autre convertible)
+ * - k   : nombre de clusters
+ * - iter: nb max d'itérations
+ *
+ * Retour :
+ * - Mat labelsImg (H x W) de type CV_32S, où labelsImg(y,x) ∈ {0,...,k-1}
+ *
+ * Remarque importante :
+ * - Ici on fait un clustering dans un feature space 5D : (B,G,R, λ*x~, λ*y~)
+ *   -> permet plus de cohérence spatiale qu'uniquement la couleur.
+ * - Pour faire couleur seule : mettre LAMBDA = 0.f.
+ */
 Mat kmeans_perso(Mat img, int k, int iter){
-    int H = img.rows;
-    int L = img.cols;
-    double lambda = 1;
-    Mat fsp(H*L, 5, CV_32F);
-    for(i=0, i<L, i++){
-        for(j=0, j<H, j++){
-            fsp.at<float>(i*H+j,0) = img.at<float>(i,j,0);
-        }
+    const int H = img.rows;
+    const int W = img.cols;
+    const int N = H * W;
+
+    // Paramètre spatial (à ajuster)
+    const float LAMBDA = 120.0f;
+
+    // Préparer sortie labels
+    Mat labels(N, 1, CV_32S, cv::Scalar(0));
+
+    // Centres : k x 5 en float
+    Mat centers(k, 5, CV_32F);
+
+    Mat bgr8 = img;
+
+    // RNG
+    cv::RNG rng((uint64)cv::getTickCount());
+
+    // Initialisation : choisir k pixels aléatoires comme centres init
+    // features = (B,G,R, λ*x~, λ*y~)
+    for (int c = 0; c < k; ++c) {
+        int idx = rng.uniform(0, N);
+        int y = idx / W;
+        int x = idx - y * W;
+
+        const cv::Vec3b pix = bgr8.at<cv::Vec3b>(y, x);
+        float* ctr = centers.ptr<float>(c);
+        ctr[0] = (float)pix[0];
+        ctr[1] = (float)pix[1];
+        ctr[2] = (float)pix[2];
+        ctr[3] = LAMBDA * (W > 1 ? (float)x / (float)(W - 1) : 0.0f);
+        ctr[4] = LAMBDA * (H > 1 ? (float)y / (float)(H - 1) : 0.0f);
     }
-    min_b=min(fsp);
-    max_b=max(fsp);
+
+    Mat newCenters(k, 5, CV_32F);
+    std::vector<int> counts(k, 0);
+
+    for (int it = 0; it < iter; ++it) {
+        bool changed = false;
+
+        for (int i = 0; i < N; ++i) {
+            int y = i / W;
+            int x = i - y * W;
+
+            const cv::Vec3b pix = bgr8.at<cv::Vec3b>(y, x);
+            //features du pixel
+            float f[5];
+            f[0] = (float)pix[0];
+            f[1] = (float)pix[1];
+            f[2] = (float)pix[2];
+            f[3] = LAMBDA * (W > 1 ? (float)x / (float)(W - 1) : 0.0f);
+            f[4] = LAMBDA * (H > 1 ? (float)y / (float)(H - 1) : 0.0f);
+
+            int best = 0;
+            float bestD = std::numeric_limits<float>::infinity();
+            
+            //trouver le centre le plus proche du pixel
+            for (int c = 0; c < k; ++c) {
+                const float* ctr = centers.ptr<float>(c);
+                float d2 = dist2_5(f, ctr);
+                if (d2 < bestD) { 
+                    bestD = d2; best = c; 
+                }
+            }
 
 
-    //Sinon :
-    Mat niv_gris(H,L,5, CV_32F);
-    for(i=0, i<L, i++){
-        for(j=0, j<H, j++){
-            niv_gris(i,j)=0.0722*img.at<float>(i,j,0)+0.7152*img.at<float>(i,j,1)+0.2126*img.at<float>(i,j,2)
+            //attribuer le centre au pixel
+            int& lab = labels.at<int>(i, 0);
+            if (lab != best) { 
+                lab = best; changed = true; 
+            }
         }
+
+        newCenters.setTo(0);
+        std::fill(counts.begin(), counts.end(), 0);
+
+        for (int i = 0; i < N; ++i) {
+            int c = labels.at<int>(i, 0);
+            int y = i / W;
+            int x = i - y * W;
+
+            const cv::Vec3b pix = bgr8.at<cv::Vec3b>(y, x);
+
+            float* acc = newCenters.ptr<float>(c);
+            acc[0] += (float)pix[0];
+            acc[1] += (float)pix[1];
+            acc[2] += (float)pix[2];
+            acc[3] += LAMBDA * (W > 1 ? (float)x / (float)(W - 1) : 0.0f);
+            acc[4] += LAMBDA * (H > 1 ? (float)y / (float)(H - 1) : 0.0f);
+
+            counts[c]++;
+        }
+
+        for (int c = 0; c < k; ++c) {
+            float* ctr = newCenters.ptr<float>(c);
+            if (counts[c] > 0) {
+                float inv = 1.f / (float)counts[c];
+                for (int j = 0; j < 5; ++j) {
+                    ctr[j] *= inv;
+                }
+            } else {
+                int idx = rng.uniform(0, N);
+                int y = idx / W;
+                int x = idx - y * W;
+                const cv::Vec3b pix = bgr8.at<cv::Vec3b>(y, x);
+
+                ctr[0] = (float)pix[0];
+                ctr[1] = (float)pix[1];
+                ctr[2] = (float)pix[2];
+                ctr[3] = LAMBDA * (W > 1 ? (float)x / (float)(W - 1) : 0.0f);
+                ctr[4] = LAMBDA * (H > 1 ? (float)y / (float)(H - 1) : 0.0f);
+            }
+        }
+
+        //si labels n'ont pas changé, on stop
+        centers = newCenters.clone();
+        if (!changed) break;
     }
+
+    Mat labelsImg = labels.reshape(1, H); // CV_32S, HxW
+    return labelsImg;
 }
 
 int main(int argc, char** argv)
@@ -139,8 +264,8 @@ int main(int argc, char** argv)
 
     // 1) to call kmeans we need to first convert the image into floats (CV_32F)
     // see the method Mat.convertTo()
-    Mat gt16 = imread(groundTruthFilename,IMREAD_GRAYSCALE);
     m = imread(imageFilename);
+    PRINT_MAT_INFO(m);
     Mat convertM;
     m.convertTo(convertM,CV_32F);
 
@@ -157,36 +282,64 @@ int main(int argc, char** argv)
 
     // now we can call kmeans(...)
     double res = kmeans(v,k,labels,TermCriteria(TermCriteria::MAX_ITER,iterations,epsilon),3,KMEANS_RANDOM_CENTERS,centers);
+    Mat labelsPerso = kmeans_perso(m,2,100);
+
+    PRINT_MAT_INFO(labelsPerso);
 
     Mat labels2D = labels.reshape(1,m.rows);
 
     Mat mask255;
+    Mat perso255;
     labels2D.convertTo(mask255, CV_8U, 255.0);
+    labelsPerso.convertTo(perso255, CV_8U, 255.0);
 
     imwrite("../res/fond_forme.png",mask255);
+    imwrite("../res/fond_forme_perso.png",perso255);
     Mat gt;
-    gt16.convertTo(gt, CV_8U);
-    cout<<"type gt = "<<gt.type()<<endl;
-    cout<<"type pred = "<<mask255.type()<<endl;
+    if (!groundTruthFilename.empty()){
+        Mat gt16 = imread(groundTruthFilename,IMREAD_GRAYSCALE);
+        gt16.convertTo(gt, CV_8U);
+        cout<<"type gt = "<<gt.type()<<endl;
+        cout<<"type pred = "<<mask255.type()<<endl;
+        cout<<"type pred = "<<perso255.type()<<endl;
+        //Kmeans OpenCV
+        Mat pred_pos = (mask255>0);
+        Mat gt_pos = (gt==0);
 
-    Mat pred_pos = (mask255>0);
-    Mat gt_pos = (gt==0);
+        Mat TPmat = pred_pos & gt_pos;
+        Mat FPmat = pred_pos & (~gt_pos);
+        Mat FNmat = (~pred_pos) & gt_pos;
 
-    Mat TPmat = pred_pos & gt_pos;
-    Mat FPmat = pred_pos & (~gt_pos);
-    Mat FNmat = (~pred_pos) & gt_pos;
+        double TP=static_cast<double>(countNonZero(TPmat));
+        double FP=static_cast<double>(countNonZero(FPmat));
+        double FN=static_cast<double>(countNonZero(FNmat));
 
-    double TP=static_cast<double>(countNonZero(TPmat));
-    double FP=static_cast<double>(countNonZero(FPmat));
-    double FN=static_cast<double>(countNonZero(FNmat));
+        double P = div(TP,TP+FP);
+        double S = div(TP,TP+FN);
+        double DSC = div(2.0*TP,2.0*TP+FP+FN);
+        cout<< "OPENCV"<<endl;
+        cout<< "P = "<< P <<endl;
+        cout<< "S = "<< S <<endl;
+        cout<< "DSC = "<< DSC <<endl;
+        //Kmeans Perso
+        pred_pos = (perso255>0);
 
-    double P = div(TP,TP+FP);
-    double S = div(TP,TP+FN);
-    double DSC = div(2.0*TP,2.0*TP+FP+FN);
+        TPmat = pred_pos & gt_pos;
+        FPmat = pred_pos & (~gt_pos);
+        FNmat = (~pred_pos) & gt_pos;
 
-    cout<< "P = "<< P <<endl;
-    cout<< "S = "<< S <<endl;
-    cout<< "DSC = "<< DSC <<endl;
+        TP=static_cast<double>(countNonZero(TPmat));
+        FP=static_cast<double>(countNonZero(FPmat));
+        FN=static_cast<double>(countNonZero(FNmat));
+
+        P = div(TP,TP+FP);
+        S = div(TP,TP+FN);
+        DSC = div(2.0*TP,2.0*TP+FP+FN);
+        cout<< "Perso"<<endl;
+        cout<< "P = "<< P <<endl;
+        cout<< "S = "<< S <<endl;
+        cout<< "DSC = "<< DSC <<endl;
+    }
 
     return EXIT_SUCCESS;
 }
